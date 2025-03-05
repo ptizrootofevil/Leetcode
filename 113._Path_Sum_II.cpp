fe9f5aad7ea57db9ec70d308545179200dcc2015
ptizrootofevil <148359768+ptizrootofevil@users.noreply.github.com>
2025-03-05 11:19:14 +0300

    Create 113._Path_Sum_II.cpp

---
+++ 113._Path_Sum_II.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> res;
+        vector<int> path;
+        hasPathSum(root, targetSum, path, &res);
+        return res;
+    }
+
+    void hasPathSum(TreeNode* root, int targetSum, vector<int> path, vector<vector<int>>* res) {
+        if (!root) return;
+        targetSum -= root->val;
+        path.push_back(root->val);
+        if (targetSum == 0 && !root->left && !root->right) res->push_back(path);
+        hasPathSum(root->left, targetSum, path, res); 
+        hasPathSum(root->right, targetSum, path, res);
+    }
+};
